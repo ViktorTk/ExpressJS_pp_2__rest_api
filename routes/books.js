6ac39e1fc6a771ef0d8b562a2cf80b161724c375
Viktor <viktor.avgn@yandex.ru>
2026-05-11 20:46:22 +0700

    реализация GET-запроса по ID + 404 статус

--- routes/books.js
+++ routes/books.js
@@ -7,10 +7,28 @@ let books = [
     author: 'John Doe',
     title: 'JavaScript Book',
   },
+  {
+    id: 2,
+    author: 'Doe John',
+    title: 'TypeScript Book',
+  },
 ]
 
 router.get('/', (req, res) => {
   res.json(books)
+})
+
+router.get('/:id', (req, res) => {
+  const bookId = parseInt(req.params.id, 10)
+  const book = books.find((book) => book.id === bookId)
+
+  if (book) {
+    res.json(book)
+  }
+
+  return res.status(404).json({
+    status: `Book with ${bookId} not found`,
+  })
 })
 
 module.exports = router
